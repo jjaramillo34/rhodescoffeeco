56bd6a686a9ea66503c41a146687c76cbe5748ed
Javier Jaramillo <jjaramillo34@gmail.com>
2023-08-20 12:42:37 -0400

    [chore]: Add @vercel/analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import Preloader from "@/src/layouts/Preloader";
 import "@/styles/globals.css";
+// add vercel analytics
+import { Analytics } from "@vercel/analytics";
+
 import { Fragment } from "react";
 const App = ({ Component, pageProps }) => {
   return (
     <Fragment>
       <Preloader />
+      <Analytics />
       <Component {...pageProps} />
     </Fragment>
   );
